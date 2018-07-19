6086fe8964e59b0615e6e27185219ad7b6fef318
milikne <milicaknezevic13@gmail.com>
2018-07-19 14:10:59 +0200

    refactor project

--- online-survey-creator/src/main/java/rs/levi9/team1/survey/repository/SurveyOptionsRepository.java
+++
@@ -1,9 +0,0 @@
-package rs.levi9.team1.survey.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rs.levi9.team1.survey.domain.SurveyOptions;
-
-@Repository
-public interface SurveyOptionsRepository extends JpaRepository<SurveyOptions, Long> {
-}
