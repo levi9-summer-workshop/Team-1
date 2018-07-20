0196c06cac493cac4303e45a3a15689755767bb3
milancvetic <milan.cvetic@live.com>
2018-07-20 15:37:03 +0200

    minor updates on home component

--- online-survey-creator-buisiness/src/main/resources/data.sql
+++ online-survey-creator-buisiness/src/main/resources/data.sql
@@ -90,3 +90,52 @@ INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answ
 INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer4',4);
 INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer5',4);
 
+INSERT INTO `survey_creator`.`survey`(`survey_description`,`survey_expire_date`,`fk_survey_user`,`fk_survey_status`,`fk_survey_privacy`)VALUES('Description of demo Survey4','2018-10-17',1,1,1);
+
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question1',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question2',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('MULTIPLE_ANSWERS','Question3',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question4',1);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer4',1);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',2);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',2);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',2);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',3);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',3);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer4',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer5',4);
+
+INSERT INTO `survey_creator`.`survey`(`survey_description`,`survey_expire_date`,`fk_survey_user`,`fk_survey_status`,`fk_survey_privacy`)VALUES('Description of demo Survey5','2018-10-17',1,1,1);
+
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question1',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question2',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('MULTIPLE_ANSWERS','Question3',1);
+INSERT INTO `survey_creator`.`survey_question`(`question_type`,`text`,`fk_survey`) VALUES ('SINGLE_ANSWER','Question4',1);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',1);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer4',1);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',2);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',2);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',2);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',3);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',3);
+
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer1',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer2',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer3',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer4',4);
+INSERT INTO `survey_creator`.`survey_answer`(`text`,`fk_question`) VALUES ('Answer5',4);
