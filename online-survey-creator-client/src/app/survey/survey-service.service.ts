440fa0ab3f4feb954d2259389f26c9eeb8ab4d6d
dzodzo7 <jovan.dzo@gmail.com>
2018-07-26 16:29:54 +0200

    implement create survey page frontend and backend

--- online-survey-creator-client/src/app/survey/survey-service.service.ts
+++ online-survey-creator-client/src/app/survey/survey-service.service.ts
@@ -6,7 +6,7 @@ import {
 import { Router } from '@angular/router';
 import 'rxjs'; // tslint:disable-line
 import { AuthService } from '../login/auth-service.service';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/RX';
 import { Survey } from './survey.model';
 
 @Injectable()
@@ -21,23 +21,27 @@ export class SurveyService {
   }
 
   getPublicSurveys(headers: HttpHeaders): Observable<Survey[]> {
-    return this.httpClient.get<Survey[]>(this.API + "privacy/public", { headers: headers });
+    return this.httpClient.get<Survey[]>(this.API + 'privacy/public', { headers: headers });
   }
 
-  getUserSurveys(id: number): Observable<Survey[]>{
-    return this.httpClient.get<Survey[]>(this.API + "user/" + id, { headers: this.authService.getAuthHeaders()});
+  getUserSurveys(id: number): Observable<Survey[]> {
+    return this.httpClient.get<Survey[]>(this.API + 'user/' + id, { headers: this.authService.getAuthHeaders()});
   }
 
-  deleteSurvey(id: number){
+  deleteSurvey(id: number) {
     return this.httpClient.delete(this.API + id, { headers: this.authService.getAuthHeaders()});
   }
 
-  closeSurvey(survey: Survey){
-    return this.httpClient.put(this.API, survey ,{ headers: this.authService.getAuthHeaders()});
+  closeSurvey(survey: Survey) {
+    return this.httpClient.put(this.API, survey , { headers: this.authService.getAuthHeaders()});
   }
 
-  getSurveyById(id: number): Observable<Survey>{
-    return this.httpClient.get<Survey>(this.API + id,{ headers: this.authService.getAuthHeaders()});
+  getSurveyById(id: number): Observable<Survey> {
+    return this.httpClient.get<Survey>(this.API + id, { headers: this.authService.getAuthHeaders()});
+  }
+
+  saveSurvey(survey: Survey) {
+    return this.httpClient.post(this.API, survey, { headers: this.authService.getAuthHeaders()});
   }
 
 }
