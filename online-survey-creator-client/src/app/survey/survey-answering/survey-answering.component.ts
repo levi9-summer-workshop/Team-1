3fc7462291023c79ea80527a424c17557be9bbd4
milancvetic <milan.cvetic@live.com>
2018-07-26 15:52:38 +0200

    updates on aswering survey

--- online-survey-creator-client/src/app/survey/survey-answering/survey-answering.component.ts
+++ online-survey-creator-client/src/app/survey/survey-answering/survey-answering.component.ts
@@ -2,28 +2,35 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Survey } from '../survey.model';
 import { SurveyService } from '../survey-service.service';
 import { NgForm, FormControl, FormGroup } from '../../../../node_modules/@angular/forms';
+import { Answer } from '../../answer/answer.model';
+import { forEach } from '../../../../node_modules/@angular/router/src/utils/collection';
 @Component({
   selector: 'survey-answering',
   templateUrl: './survey-answering.component.html',
   styleUrls: ['./survey-answering.component.css']
 })
 export class SurveyAnsweringComponent implements OnInit {
-
+  
   currentSurvey: Survey;
   buttonType: string;
-  selectedAnswer: boolean;
+  selectedAnswers: number[] = [];
 
   constructor(private surveyService: SurveyService) { }
 
   ngOnInit() {
     this.currentSurvey = new Survey (null, null, null, null, null, null, null);
     this.surveyService.getSurveyById(1).subscribe(
-      (survey) => this.currentSurvey = survey
+      (survey) => { 
+        this.currentSurvey = survey;
+        console.log(this.currentSurvey);
+      }
     );
   }
 
-  // onSurveySubmit(form: NgForm) {
-  //   console.log(form.value);
-  // }
+
+
+  onSurveySubmit(form: NgForm) {      
+    console.log(form.value);
+  }
 
 }
