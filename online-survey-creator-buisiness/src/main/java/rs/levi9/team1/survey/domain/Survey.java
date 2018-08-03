63e7e97c110323a2806cb74127125813a5ead8ce
milancvetic <milan.cvetic@live.com>
2018-08-03 10:08:29 +0200

    minor updates

--- online-survey-creator-buisiness/src/main/java/rs/levi9/team1/survey/domain/Survey.java
+++ online-survey-creator-buisiness/src/main/java/rs/levi9/team1/survey/domain/Survey.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Survey extends BaseEntity {
 
     @NotNull
+    @Type(type = "text")
     private String surveyDescription;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
